403970d0f15d3c458b9e9816cae2979aa185388d
Leeyoujin00 <cymn6032@gmail.com>
2023-08-16 15:17:04 +0900

    travis CI yml

--- src/main/java/com/hisujung/web/jpa/chating/ChatingJoinInfoRepository.java
+++ src/main/java/com/hisujung/web/jpa/chating/ChatingJoinInfoRepository.java
@@ -1,7 +1,4 @@
 package com.hisujung.web.jpa.chating;
 
-import com.hisujung.web.entity.chating.ChatingJoinInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatingJoinInfoRepository extends JpaRepository<ChatingJoinInfo, Long> {
+public interface ChatingJoinInfoRepository {
 }
